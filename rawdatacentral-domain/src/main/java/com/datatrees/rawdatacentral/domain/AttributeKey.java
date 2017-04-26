0a949b0a9451d8464f091c6912a7983c48170c00
zhouxinghai <zhouxinghai@zhouxinghaideMacBook-Pro.local>
2017-04-26 11:11:07 +0800

    no message

--- rawdatacentral-domain/src/main/java/com/datatrees/rawdatacentral/domain/AttributeKey.java
+++ rawdatacentral-domain/src/main/java/com/datatrees/rawdatacentral/domain/AttributeKey.java
@@ -9,4 +9,7 @@ import java.io.Serializable;
 public class AttributeKey implements Serializable {
 
     public static final String CAPTCHA = "captcha";//验证码
+    public static final String STATUS = "status";//状态
+    public static final String REMARK = "remark";//备注
+
 }
