a0ea523f2df5d46d45d8bf19148bc981707db2a9
zhouxinghai <zhouxinghai@zhouxinghaideMacBook-Pro.local>
2017-05-03 11:02:56 +0800

    打包到nexus脚本

--- cmd/gradle/upload.sh
+++ cmd/gradle/upload.sh
@@ -1,5 +1,15 @@
 #!/bin/sh
+rootPath=`pwd`
+
+echo "$rootPath/rawdatacentral-api"
+cd $rootPath/rawdatacentral-api
 gradle upload
+
+echo "$rootPath/rawdatacentral-domain"
+cd $rootPath/rawdatacentral-domain
+gradle upload
+
+cd $rootPath
 
 
 
