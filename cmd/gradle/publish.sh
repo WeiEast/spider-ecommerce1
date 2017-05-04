d42889a8a565f2da185c87da37a25201b62d853b
zhouxinghai <zhouxinghai@zhouxinghaideMacBook-Pro.local>
2017-05-04 14:44:37 +0800

    打包时强制刷新jar

--- cmd/gradle/publish.sh
+++ cmd/gradle/publish.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-gradle clean build installApp -x test
+gradle clean build installApp  -x test --refresh-dependencies
 cd target/;
 for file in $(ls -rt *.tar.gz|tail -1);
 do
@@ -15,6 +15,7 @@ ssh root@116.62.242.180 << EOF
     sleep 1;
     sh restart.sh;
     echo $file;
+    tail -100f /dashu/log/rawdatacentral.log
     exit;
 EOF
 
