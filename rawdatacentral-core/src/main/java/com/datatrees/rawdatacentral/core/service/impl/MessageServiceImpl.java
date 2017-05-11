898a121a4ca5b70e13a6bf0ce2db67e8d01b59fd
zhouxinghai <zhouxinghai@zhouxinghaideMacBook-Pro.local>
2017-05-11 21:00:21 +0800

    任务日志添加websiteName

---
+++ rawdatacentral-core/src/main/java/com/datatrees/rawdatacentral/core/service/impl/MessageServiceImpl.java
@@ -0,0 +1,67 @@
+package com.datatrees.rawdatacentral.core.service.impl;
+
+import com.alibaba.rocketmq.client.producer.MQProducer;
+import com.alibaba.rocketmq.client.producer.SendResult;
+import com.alibaba.rocketmq.client.producer.SendStatus;
+import com.alibaba.rocketmq.common.message.Message;
+import com.datatrees.common.util.GsonUtils;
+import com.datatrees.crawler.core.processor.AbstractProcessorContext;
+import com.datatrees.crawler.core.processor.plugin.PluginFactory;
+import com.datatrees.rawdatacentral.core.service.MessageService;
+import com.datatrees.rawdatacentral.domain.constant.AttributeKey;
+import com.datatrees.rawdatacentral.domain.enums.TopicEnum;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Created by zhouxinghai on 2017/5/11.
+ */
+@Service
+public class MessageServiceImpl implements MessageService {
+
+    private static final Logger logger = LoggerFactory.getLogger(MessageServiceImpl.class);
+
+    @Resource
+    private MQProducer          producer;
+
+    @Override
+    public boolean sendTaskLog(String msg, String errorDetail) {
+        AbstractProcessorContext context = PluginFactory.getProcessorContext();
+        Long taskId = context.getLong(AttributeKey.TASK_ID);
+        String websiteName = context.getWebsiteName();
+        Map<String, Object> map = new HashMap<String, Object>();
+        map.put("taskId", taskId);
+        map.put("websiteName", websiteName);
+        map.put("timestamp", System.currentTimeMillis());
+        map.put("msg", msg);
+        map.put("errorDetail", errorDetail);
+
+        Message mqMessage = new Message();
+        mqMessage.setTopic(TopicEnum.TASK_LOG.getCode());
+        mqMessage.setBody(GsonUtils.toJson(map).getBytes());
+
+        int retry = 0;
+        while (retry++ <= 3) {
+            try {
+                SendResult sendResult = producer.send(mqMessage);
+                logger.info("send result message={},status={}", GsonUtils.toJson(map), sendResult.getSendStatus());
+                if (sendResult != null && SendStatus.SEND_OK.equals(sendResult.getSendStatus())) {
+                    return true;
+                }
+            } catch (Exception e) {
+                logger.error(e.getMessage(), e);
+            }
+        }
+        return false;
+    }
+
+    @Override
+    public boolean sendTaskLog(String msg) {
+        return sendTaskLog(msg, null);
+    }
+}
