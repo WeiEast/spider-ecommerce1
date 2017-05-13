6e3a10f156bd3204c8ec8f144d54d7873ca2d1c6
zhouxinghai <zhouxinghai@zhouxinghaideMacBook-Pro.local>
2017-05-13 10:53:59 +0800

    去掉本地线上oss账号密码

--- rawdatacentral-submitter/src/main/java/com/datatrees/rawdatacentral/submitter/common/SubmitConstant.java
+++ rawdatacentral-submitter/src/main/java/com/datatrees/rawdatacentral/submitter/common/SubmitConstant.java
@@ -17,10 +17,10 @@ public interface SubmitConstant {
     int SUBMITTER_UPLOAD_CORE_THREAD_NUM = PropertiesConfiguration.getInstance().getInt("submitter.upload.corePoolSize", 5);
     int SUBMITTER_UPLOAD_MAX_THREAD_NUM = PropertiesConfiguration.getInstance().getInt("submitter.upload.maximumPoolSize", 60);
     int SUBMITTER_UPLOAD_MAX_TASK_NUM = PropertiesConfiguration.getInstance().getInt("submitter.upload.maximumTaskNum", 10);
-    String ALIYUN_OSS_ENDPOINT = PropertiesConfiguration.getInstance().get("submitter.aliyun.ossEndpoint", "http://oss-cn-hangzhou.aliyuncs.com");
-    String ALIYUN_OSS_ACCESSID = PropertiesConfiguration.getInstance().get("submitter.aliyun.ossAccessId", "ewZN3ik4X0S5azYd");
-    String ALIYUN_OSS_ACCESSSECRET = PropertiesConfiguration.getInstance().get("submitter.aliyun.ossAccessSecret", "pZOQuEJVDO2tHKnIamnnVRSpazYmcE");
-    String ALIYUN_OSS_DEFAULTBUCKET = PropertiesConfiguration.getInstance().get("submitter.aliyun.ossDefaultBucket", "gongfu2");
+    String ALIYUN_OSS_ENDPOINT = PropertiesConfiguration.getInstance().get("submitter.aliyun.ossEndpoint", "");
+    String ALIYUN_OSS_ACCESSID = PropertiesConfiguration.getInstance().get("submitter.aliyun.ossAccessId", "");
+    String ALIYUN_OSS_ACCESSSECRET = PropertiesConfiguration.getInstance().get("submitter.aliyun.ossAccessSecret", "");
+    String ALIYUN_OSS_DEFAULTBUCKET = PropertiesConfiguration.getInstance().get("submitter.aliyun.ossDefaultBucket", "");
     String SUBMITTER_NEEDUPLOAD_KEY = PropertiesConfiguration.getInstance().get("submitter.needUpload.keys", "mailHeader,pageContent,attachment");
 
 }
