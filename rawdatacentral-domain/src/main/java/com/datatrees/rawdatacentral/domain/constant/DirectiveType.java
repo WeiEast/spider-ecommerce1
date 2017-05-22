232b07e489f17e1a46db8fffd1b0c440b8060432
zhouxinghai <zhouxinghai@zhouxinghaideMacBook-Pro.local>
2017-05-22 11:55:00 +0800

    no message

---
+++ rawdatacentral-domain/src/main/java/com/datatrees/rawdatacentral/domain/constant/DirectiveType.java
@@ -0,0 +1,13 @@
+package com.datatrees.rawdatacentral.domain.constant;
+
+/**
+ * 交互指令类型
+ * Created by zhouxinghai on 2017/5/22.
+ */
+public class DirectiveType {
+
+    /**
+     * 将后端失败率高的页面交给APP抓取
+     */
+    public static final String GRAB_URL = "grab_url";
+}
