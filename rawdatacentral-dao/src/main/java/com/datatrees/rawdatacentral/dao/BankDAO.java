667ac3dc90e3675e41d48aa9dfb78bd79d867377
周兴海 <zhouxinghai@treefinance.com.cn>
2017-06-26 20:03:24 +0800

    no message

--- rawdatacentral-dao/src/main/java/com/datatrees/rawdatacentral/dao/BankDAO.java
+++
@@ -1,9 +0,0 @@
-package com.datatrees.rawdatacentral.dao;
-
-import com.datatrees.rawdatacentral.dao.mapper.BankMapper;
-import javax.annotation.Resource;
-
- /** create by system from table t_bank(Bank basic info)  */
-@Resource
-public interface BankDAO extends BankMapper {
-}
