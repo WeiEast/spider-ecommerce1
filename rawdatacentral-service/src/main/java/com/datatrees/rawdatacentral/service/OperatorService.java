0375faa9c352394e0a4c43a8e8a233162ae01911
周兴海 <zhouxinghai@treefinance.com.cn>
2017-06-27 20:53:54 +0800

    operator 替换

---
+++ rawdatacentral-service/src/main/java/com/datatrees/rawdatacentral/service/OperatorService.java
@@ -0,0 +1,17 @@
+package com.datatrees.rawdatacentral.service;
+
+import com.datatrees.rawdatacentral.domain.model.Operator;
+
+/**
+ * 运营商目录
+ * Created by zhouxinghai on 2017/6/27.
+ */
+public interface OperatorService {
+
+    /**
+     *
+     * @param websiteId
+     * @return
+     */
+    public Operator getByWebsiteId(Integer websiteId);
+}
---
+++ rawdatacentral-service/src/main/java/com/datatrees/rawdatacentral/service/impl/OperatorServiceImpl.java
@@ -0,0 +1,51 @@
+package com.datatrees.rawdatacentral.service.impl;
+
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+import javax.annotation.Resource;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Service;
+
+import com.datatrees.rawdatacentral.dao.OperatorDAO;
+import com.datatrees.rawdatacentral.domain.model.Operator;
+import com.datatrees.rawdatacentral.domain.model.example.OperatorExample;
+import com.datatrees.rawdatacentral.service.OperatorService;
+import com.datatrees.rawdatacentral.share.RedisService;
+
+/**
+ * Created by zhouxinghai on 2017/6/27.
+ */
+@Service
+public class OperatorServiceImpl implements OperatorService {
+
+    private static final Logger logger = LoggerFactory.getLogger(OperatorServiceImpl.class);
+
+    @Resource
+    private RedisService        redisService;
+
+    @Resource
+    private OperatorDAO         operatorDAO;
+
+    @Override
+    public Operator getByWebsiteId(Integer websiteId) {
+        if (null == websiteId) {
+            logger.warn("invalid param websiteId is null");
+            return null;
+        }
+        String key = "rawdatacentral_operator_websiteid_" + websiteId;
+        Operator operator = redisService.getCache(key, Operator.class);
+        if (null == operator) {
+            OperatorExample example = new OperatorExample();
+            example.createCriteria().andWebsiteidEqualTo(websiteId).andIsenabledEqualTo(true);
+            List<Operator> list = operatorDAO.selectByExample(example);
+            if (null != list && !list.isEmpty()) {
+                operator = list.get(0);
+                redisService.cache(key, operator, 1, TimeUnit.HOURS);
+            }
+        }
+        return operator;
+    }
+}
