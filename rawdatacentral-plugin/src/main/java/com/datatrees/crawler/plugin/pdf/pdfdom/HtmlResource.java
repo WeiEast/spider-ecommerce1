17e219bc6a1b5d7eda05062fa07475a65319e5c4
周兴海 <zhouxinghai@treefinance.com.cn>
2017-07-05 15:25:43 +0800

    AbstractPlugin迁移到rawdatacentral

---
+++ rawdatacentral-plugin/src/main/java/com/datatrees/crawler/plugin/pdf/pdfdom/HtmlResource.java
@@ -0,0 +1,23 @@
+package com.datatrees.crawler.plugin.pdf.pdfdom;
+
+import java.io.IOException;
+
+public abstract class HtmlResource {
+    protected String name;
+
+    public HtmlResource(String name)
+    {
+        this.name = name;
+    }
+
+    public abstract byte[] getData() throws IOException;
+
+    public abstract String getFileEnding();
+
+    public abstract String getMimeType();
+
+    public String getName()
+    {
+        return name;
+    }
+}
