9a50a8176e9d205ccc54a5b9dd82e081ad971142
周兴海 <1093320666@qq.com>
2017-08-22 20:27:41 +0800

    no message

--- rawdatacentral-common/src/main/java/com/datatrees/rawdatacentral/common/utils/TemplateUtils.java
+++ rawdatacentral-common/src/main/java/com/datatrees/rawdatacentral/common/utils/TemplateUtils.java
@@ -1,15 +1,24 @@
 package com.datatrees.rawdatacentral.common.utils;
 
 import org.apache.commons.lang3.StringUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * Created by zhouxinghai on 2017/7/14.
  */
 public class TemplateUtils {
 
+    private static final Logger logger = LoggerFactory.getLogger(TemplateUtils.class);
+
     public static String format(String template, Object... args) {
         if (StringUtils.isNoneBlank(template) && args.length > 0) {
-            for (Object arg : args) {
+            for (int i = 0; i < args.length; i++) {
+                Object arg = args[i];
+                if (null == arg) {
+                    logger.error("arg is null template={},index={}", template, i);
+                    throw new RuntimeException("format error arg is null");
+                }
                 template = template.replaceFirst("\\{}", String.valueOf(arg));
             }
             template = template.replaceAll("\\{}", "");
