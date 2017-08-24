fb8e110c75105ed85122b2ee16e9343b3d2204f4
周兴海 <1093320666@qq.com>
2017-08-24 14:51:40 +0800

    上传脚本bugfix

--- upload/upload_dev.sh
+++ upload/upload_dev.sh
@@ -1,8 +1,7 @@
 #!/bin/sh
 url="http://192.168.5.15:6789/website/uploadPluginJar"
 gradle clean install -x test
-cd rawdatacentral-plugin-operator/build/libs/
-list=`find . -name '*.jar' | grep -v 'sources'`
+list=`find rawdatacentral-plugin-operator/build/libs/rawdatacentral-plugin-operator.jar -name '*.jar' | grep -v 'sources'`
 for file in $list
 do
     curl  -F "jar=@$file" $url
