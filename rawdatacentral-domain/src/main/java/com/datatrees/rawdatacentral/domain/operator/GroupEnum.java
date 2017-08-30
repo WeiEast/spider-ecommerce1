73c272253308acdef2f6e4c0388b6bddfa64d508
周兴海 <1093320666@qq.com>
2017-08-30 17:15:55 +0800

    这个就不要动了

--- rawdatacentral-domain/src/main/java/com/datatrees/rawdatacentral/domain/operator/GroupEnum.java
+++ rawdatacentral-domain/src/main/java/com/datatrees/rawdatacentral/domain/operator/GroupEnum.java
@@ -14,13 +14,8 @@ package com.datatrees.rawdatacentral.domain.operator;
  */
 public enum GroupEnum {
 
-    //    CHINA_10010("CHINA_10010","中国联通","china_10010_app"),
-    CHINA_10010("CHINA_10010", "中国联通", "china_10010_web"),
+    CHINA_10010("CHINA_10010", "中国联通", "china_10010_app"),
     ZHE_JIANG_10086("ZHE_JAING_10086", "浙江移动", "zhe_jiang_10086_web"),
-    //测试
-    //ZHE_JIANG_10086("ZHE_JAING_10086", "浙江移动", "china_10086_shop"),
-    //    ZHE_JIANG_10000("ZHE_JAING_10000","浙江电信","zhe_jiang_10086_web"),
-    //测试
     ZHE_JIANG_10000("ZHE_JAING_10000", "浙江电信", "china_10000_app"),
     GUANG_DONG_10086("GUANG_DONG_10086", "广东移动", "guang_dong_10086_web"),
     GUANG_DONG_10000("GUANG_DONG_10000", "广东电信", "guang_dong_10000_web"),
