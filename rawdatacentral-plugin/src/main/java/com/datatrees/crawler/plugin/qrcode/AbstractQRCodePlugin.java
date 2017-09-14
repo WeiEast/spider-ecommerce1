2c55057507513de117c2e9d9f5c1506b9fccd4ef
周兴海 <1093320666@qq.com>
2017-09-14 11:16:06 +0800

    详单和二维码校验日志

--- rawdatacentral-plugin/src/main/java/com/datatrees/crawler/plugin/qrcode/AbstractQRCodePlugin.java
+++ rawdatacentral-plugin/src/main/java/com/datatrees/crawler/plugin/qrcode/AbstractQRCodePlugin.java
@@ -13,6 +13,8 @@ import com.datatrees.crawler.core.processor.common.exception.ResultEmptyExceptio
 import com.datatrees.crawler.core.processor.plugin.PluginConstants;
 import com.datatrees.crawler.core.processor.plugin.PluginFactory;
 import com.datatrees.crawler.plugin.AbstractRawdataPlugin;
+import com.datatrees.rawdatacentral.api.MonitorService;
+import com.datatrees.rawdatacentral.common.utils.BeanFactoryUtils;
 import com.datatrees.rawdatacentral.domain.constant.AttributeKey;
 import com.datatrees.rawdatacentral.domain.constant.DirectiveRedisCode;
 import com.datatrees.rawdatacentral.domain.constant.DirectiveType;
@@ -26,7 +28,8 @@ import org.slf4j.LoggerFactory;
 public abstract class AbstractQRCodePlugin extends AbstractRawdataPlugin implements QRCodeVerification {
 
     int verifyQRCodeCount = 0;
-    private Logger logger = LoggerFactory.getLogger(AbstractQRCodePlugin.class);
+    private Logger         logger         = LoggerFactory.getLogger(AbstractQRCodePlugin.class);
+    private MonitorService monitorService = BeanFactoryUtils.getBean(MonitorService.class);
 
     @Override
     public String process(String... args) throws Exception {
@@ -60,6 +63,7 @@ public abstract class AbstractQRCodePlugin extends AbstractRawdataPlugin impleme
         int maxRetry = 0;
         //当前重试次数
         int retry = 0;
+        monitorService.sendTaskLog(taskId, "二维码插件启动");
         getMessageService().sendTaskLog(taskId, "等待用户扫描二维码");
         //保存交互指令到redis,让APP端轮询进入等待模式
         DirectiveResult<String> sendDirective = new DirectiveResult<>(DirectiveType.CRAWL_QR, taskId);
@@ -69,9 +73,11 @@ public abstract class AbstractQRCodePlugin extends AbstractRawdataPlugin impleme
             String qrcodeResult = refreshQRCode(paramsMap);
             logger.info("do refresh qrcode  retry={},taskId={},websiteName={}", retry, taskId, websiteName);
             if (StringUtils.isEmpty(qrcodeResult)) {
+                monitorService.sendTaskLog(taskId, "二维码插件-->二维码-->刷新失败!");
                 logger.error("request qrCode error!retry={},taskId={},websiteName={}", retry, taskId, websiteName);
                 continue;
             }
+            monitorService.sendTaskLog(taskId, "二维码插件-->二维码-->刷新成功,等待用户扫描二维码");
             directiveId = getMessageService().sendDirective(taskId, DirectiveEnum.REQUIRE_QR.getCode(), qrcodeResult);
             getRedisService().saveDirectiveResult(directiveId, sendDirective);
 
@@ -90,6 +96,7 @@ public abstract class AbstractQRCodePlugin extends AbstractRawdataPlugin impleme
                     getRedisService().saveDirectiveResult(directiveId, sendDirective);
                     logger.info("verifyQRCodeStatus taskId={},directiveId={},websiteName={},status = {}", taskId, directiveId, websiteName,
                             DirectiveRedisCode.SCANNED);
+                    monitorService.sendTaskLog(taskId, "二维码插件-->二维码-->用户已扫描,等待用户确认!");
                 }
                 if (QRCodeStatus.CONFIRMED == verifyResult.status) {
                     QRCodeResult confirmResult = confirmQRCodeStatus(paramsMap);
@@ -101,12 +108,14 @@ public abstract class AbstractQRCodePlugin extends AbstractRawdataPlugin impleme
                             sendDirective.fill(DirectiveRedisCode.SUCCESS, null);
                             getRedisService().saveDirectiveResult(directiveId, sendDirective);
                             getMessageService().sendTaskLog(taskId, "二维码验证成功");
+                            monitorService.sendTaskLog(taskId, "二维码插件-->二维码-->校验成功!");
                             return resultMap;
                         }
                     }
                 }
             }
         } while (retry++ < maxRetry);
+        monitorService.sendTaskLog(taskId, "二维码插件-->二维码-->校验失败,任务即将终止!");
         getMessageService().sendTaskLog(taskId, "二维码验证失败");
 
         if (StringUtils.isNoneBlank(directiveId)) {
