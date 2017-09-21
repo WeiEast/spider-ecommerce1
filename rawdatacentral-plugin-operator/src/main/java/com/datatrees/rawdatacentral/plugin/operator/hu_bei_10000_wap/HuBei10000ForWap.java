df27cb88514920792de36213e18dc472e51351d6
guimeichao <guimeichao@treefinance.com.cn>
2017-09-21 09:24:24 +0800

    湖北电信登录独立

--- rawdatacentral-plugin-operator/src/main/java/com/datatrees/rawdatacentral/plugin/operator/hu_bei_10000_wap/HuBei10000ForWap.java
+++ rawdatacentral-plugin-operator/src/main/java/com/datatrees/rawdatacentral/plugin/operator/hu_bei_10000_wap/HuBei10000ForWap.java
@@ -1,8 +1,10 @@
 package com.datatrees.rawdatacentral.plugin.operator.hu_bei_10000_wap;
 
+import java.net.URLEncoder;
 import java.util.Map;
 
 import com.datatrees.rawdatacentral.common.http.TaskHttpClient;
+import com.datatrees.rawdatacentral.common.utils.CheckUtils;
 import com.datatrees.rawdatacentral.common.utils.TemplateUtils;
 import com.datatrees.rawdatacentral.domain.constant.FormType;
 import com.datatrees.rawdatacentral.domain.enums.ErrorCode;
@@ -11,8 +13,8 @@ import com.datatrees.rawdatacentral.domain.operator.OperatorParam;
 import com.datatrees.rawdatacentral.domain.result.HttpResult;
 import com.datatrees.rawdatacentral.domain.vo.Response;
 import com.datatrees.rawdatacentral.service.OperatorPluginService;
+import org.apache.commons.codec.binary.Base64;
 import org.apache.commons.lang3.StringUtils;
-import org.apache.http.entity.ContentType;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -56,7 +58,14 @@ public class HuBei10000ForWap implements OperatorPluginService {
 
     @Override
     public HttpResult<Map<String, Object>> submit(OperatorParam param) {
-        return null;
+        switch (param.getFormType()) {
+            case FormType.LOGIN:
+                return submitForLogin(param);
+            case FormType.VALIDATE_BILL_DETAIL:
+                return submitForBillDetail(param);
+            default:
+                return new HttpResult<Map<String, Object>>().failure(ErrorCode.NOT_SUPORT_METHOD);
+        }
     }
 
     @Override
@@ -84,14 +93,41 @@ public class HuBei10000ForWap implements OperatorPluginService {
         }
     }
 
+    private HttpResult<Map<String, Object>> submitForLogin(OperatorParam param) {
+        CheckUtils.checkNotBlank(param.getPassword(), ErrorCode.EMPTY_PASSWORD);
+        CheckUtils.checkNotBlank(param.getPicCode(), ErrorCode.EMPTY_PIC_CODE);
+        HttpResult<Map<String, Object>> result = new HttpResult<>();
+        Response response = null;
+        try {
+            String referer = "http://wap.hb.189.cn/login/login.jsp";
+            String templateUrl = "http://wap.hb.189.cn/login/doLogin.htm";
+            String templateData = "accountID={}&checkImgRandom={}&loginType=1&password={}";
+            String data = TemplateUtils.format(templateData, Base64.encodeBase64String(param.getMobile().toString().getBytes()), param.getPicCode(),
+                    Base64.encodeBase64String(param.getPassword().getBytes()));
+            response = TaskHttpClient.create(param, RequestType.POST, "hu_bei_10000_wap_002").setFullUrl(templateUrl).setRequestBody(data)
+                    .setReferer(referer).invoke();
+            String pageContent = response.getPageContent();
+            if (StringUtils.contains(pageContent, "flag\":\"1\"")) {
+                logger.info("登陆成功,param={}", param);
+                return result.success();
+            } else {
+                logger.error("登陆失败,param={},pageContent={}", param, response.getPageContent());
+                return result.failure(ErrorCode.LOGIN_UNEXPECTED_RESULT);
+            }
+        } catch (Exception e) {
+            logger.error("登陆失败,param={},response={}", param, response, e);
+            return result.failure(ErrorCode.LOGIN_ERROR);
+        }
+    }
+
     private HttpResult<Map<String, Object>> refeshSmsCodeForBillDetail(OperatorParam param) {
         HttpResult<Map<String, Object>> result = new HttpResult<>();
         Response response = null;
         try {
             String templateUrl = "http://wap.hb.189.cn/login/getSmsCode.htm?randomType=billQuery&phoneNumber={}";
-            response = TaskHttpClient.create(param, RequestType.GET, "hu_bei_10000_wap_001").setFullUrl(templateUrl,param.getMobile()).invoke();
+            response = TaskHttpClient.create(param, RequestType.GET, "hu_bei_10000_wap_003").setFullUrl(templateUrl, param.getMobile()).invoke();
             String pageContent = response.getPageContent();
-            if (StringUtils.contains(pageContent, "成功")) {
+            if (StringUtils.contains(pageContent, "flag\":\"1\"") || StringUtils.contains(pageContent, "msg\":\"系统异常")) {
                 logger.info("详单-->短信验证码-->刷新成功,param={}", param);
                 return result.success();
             } else {
@@ -101,6 +137,31 @@ public class HuBei10000ForWap implements OperatorPluginService {
         } catch (Exception e) {
             logger.error("详单-->短信验证码-->刷新失败,param={},response={}", param, response, e);
             return result.failure(ErrorCode.REFESH_SMS_ERROR);
+        }
+    }
+
+    private HttpResult<Map<String, Object>> submitForBillDetail(OperatorParam param) {
+        HttpResult<Map<String, Object>> result = new HttpResult<>();
+        Response response = null;
+        try {
+            String templateUrl = "http://wap.hb.189.cn/billQuery/checkRan.htm";
+            String templateData = "accountID={}&random={}";
+            String data = TemplateUtils
+                    .format(templateData, URLEncoder.encode(Base64.encodeBase64String(param.getMobile().toString().getBytes()), "UTF-8"),
+                            URLEncoder.encode(Base64.encodeBase64String(param.getSmsCode().getBytes()), "UTF-8"));
+            response = TaskHttpClient.create(param, RequestType.POST, "hu_bei_10000_wap_004").setFullUrl(templateUrl).setRequestBody(data).invoke();
+            String pageContent = response.getPageContent();
+
+            if (StringUtils.contains(pageContent, "flag\":\"1\"")) {
+                logger.info("详单-->校验成功,param={}", param);
+                return result.success();
+            } else {
+                logger.warn("详单-->短信验证码错误,param={}", param);
+                return result.failure(ErrorCode.VALIDATE_SMS_FAIL);
+            }
+        } catch (Exception e) {
+            logger.error("详单-->校验失败,param={},response={}", param, response, e);
+            return result.failure(ErrorCode.VALIDATE_ERROR);
         }
     }
 }
