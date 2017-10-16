43b109674185c0833463e19f7622b50e523fb929
zhouxinghai <zhouxinghai@treefinance.com.cn>
2017-10-16 16:21:20 +0800

    htmlutil加入

--- rawdatacentral-common/src/test/java/com/datatrees/rawdatacentral/common/utils/RedisUtilsTest.java
+++ rawdatacentral-common/src/test/java/com/datatrees/rawdatacentral/common/utils/RedisUtilsTest.java
@@ -1,29 +1,32 @@
 package com.datatrees.rawdatacentral.common.utils;
 
-import java.util.Map;
+import java.io.IOException;
+import java.util.List;
+import java.util.Set;
 
 import com.datatrees.rawdatacentral.common.http.TaskUtils;
-import com.datatrees.rawdatacentral.domain.enums.RedisKeyPrefixEnum;
-import org.apache.commons.lang3.StringUtils;
+import com.gargoylesoftware.htmlunit.WebClient;
+import com.gargoylesoftware.htmlunit.html.HtmlPage;
+import com.gargoylesoftware.htmlunit.util.Cookie;
 import org.junit.Test;
 
 public class RedisUtilsTest {
 
     @Test
-    public void test() {
-        RedisUtils.init("192.168.5.24", 6379, null);
-        long taskId = 93399525661634560L;
-        Map<String, String> map = null;
-        String redisKey = RedisKeyPrefixEnum.TASK_RESULT.getRedisKey(taskId);
-        String type = RedisUtils.type(redisKey);
-        if (StringUtils.equals("string", type)) {
-        } else {
-            map = RedisUtils.hgetAll(redisKey);
-        }
-        System.out.println(map);
-        //task.share.94450026213830656
-        String mobile = TaskUtils.getTaskShare(94450026213830656L, "mobile");
-        System.out.println(mobile);
+    public void test() throws IOException {
+        WebClient webClient = new WebClient();
+        webClient.getCookieManager().setCookiesEnabled(true);
+        webClient.getOptions().setCssEnabled(false);
+        webClient.getOptions().setDownloadImages(false);
+        webClient.getOptions().setJavaScriptEnabled(true);
+        HtmlPage page = webClient.getPage("https://ah.ac.10086.cn/login");
+        String content = page.asXml();
+        Set<Cookie> cookies = webClient.getCookieManager().getCookies();
+        List<com.datatrees.rawdatacentral.domain.vo.Cookie> list = TaskUtils.getCookies(cookies);
+        String spid = JsoupXpathUtils.selectFirst(content, "//form[@id='oldLogin']/input[@name='spid']/@value");
+        String title = JsoupXpathUtils.selectFirst(content, "/titel");
+        webClient.close();
+
 
     }
 
