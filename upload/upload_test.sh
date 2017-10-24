2a25574b98b8e668da06855c374a8b8fcfff0981
guimeichao <guimeichao@treefinance.com.cn>
2017-10-24 15:58:05 +0800

    脚本更新

--- upload/upload_test.sh
+++ upload/upload_test.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
-url="http://rawdatacentral.saas.test.treefinance.com.cn/website/uploadPluginJar"
+host="rawdatacentral.saas.test.treefinance.com.cn"
+
+
+url="http://$host/website/uploadPluginJar"
 gradle clean install -x test
 list=`find rawdatacentral-plugin-operator/build/libs/rawdatacentral-plugin-operator.jar -name '*.jar' | grep -v 'sources'`
 for file in $list
@@ -8,4 +11,13 @@ do
     curl  -F "jar=@$file;fileName=rawdatacentral-plugin-operator-$HOSTNAME.jar" $url
 done
 
+url="http://$host/redis/uploadFile"
+list=`find rawdatacentral-plugin-operator/src/main/resources -name '*.js'`
+for file in $list
+do
+   parent_name=`dirname $file`
+   file_name=${parent_name##*/}'.'${file##*/}
+   curl  -F "cache_file=@$file;fileName=$file_name" $url
+   echo \n
+done
 
