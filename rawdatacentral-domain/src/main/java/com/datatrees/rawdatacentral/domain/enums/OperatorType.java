ddd28cade76a303502643d651d9b953baf333ecd
zhouxinghai <zhouxinghai@treefinance.com.cn>
2017-10-26 13:40:35 +0800

    no message

--- rawdatacentral-domain/src/main/java/com/datatrees/rawdatacentral/domain/enums/OperatorType.java
+++ rawdatacentral-domain/src/main/java/com/datatrees/rawdatacentral/domain/enums/OperatorType.java
@@ -1,5 +1,8 @@
 package com.datatrees.rawdatacentral.domain.enums;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * 运营商类型
  * Created by zhouxinghai on 2017/8/31
@@ -9,6 +12,14 @@ public enum OperatorType {
     CMCC("10086", "移动"),
     TELECOM("10000", "电信"),
     UNICOM("10010", "联通"),;
+    private static final Map<String, String> map = new HashMap<>();
+
+    static {
+        for (OperationEnum e : OperationEnum.values()) {
+            map.put(e.getCode(), e.getName());
+        }
+    }
+
     /**
      * 代码
      */
@@ -29,5 +40,13 @@ public enum OperatorType {
 
     public String getName() {
         return name;
+    }
+
+    public String getName(String code) {
+        return map.get(code);
+    }
+
+    public String getName(Long code) {
+        return map.get(code + "");
     }
 }
