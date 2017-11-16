7bec2bd78a4f637ddb6102970bef0e48c8091116
zhouxinghai <zhouxinghai@treefinance.com.cn>
2017-11-16 17:03:10 +0800

    查询可用版本

--- rawdatacentral-api/src/main/java/com/datatrees/rawdatacentral/api/WebsiteGroupServiceApi.java
+++ rawdatacentral-api/src/main/java/com/datatrees/rawdatacentral/api/WebsiteGroupServiceApi.java
@@ -1,5 +1,9 @@
 package com.datatrees.rawdatacentral.api;
 
+import java.util.List;
+
+import com.datatrees.rawdatacentral.domain.model.WebsiteGroup;
+
 /**
  * 运营商分组和负载
  */
@@ -9,4 +13,11 @@ public interface WebsiteGroupServiceApi {
      * 查询可用版本数量
      */
     Integer enableCount(String groupCode);
+
+    /**
+     * 查询可用版本
+     * @param groupCode
+     * @return
+     */
+    List<WebsiteGroup> queryEnable(String groupCode);
 }
