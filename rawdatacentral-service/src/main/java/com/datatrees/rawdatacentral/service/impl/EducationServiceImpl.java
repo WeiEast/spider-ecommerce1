f1a0e14766446119ecda819eba65c8f415217d8e
zhangyanjia <zhangyanjia@treefinance.com.cn>
2017-12-25 13:58:13 +0800

    学信网开发  bugfix

--- rawdatacentral-service/src/main/java/com/datatrees/rawdatacentral/service/impl/EducationServiceImpl.java
+++ rawdatacentral-service/src/main/java/com/datatrees/rawdatacentral/service/impl/EducationServiceImpl.java
@@ -198,10 +198,15 @@ public class EducationServiceImpl implements EducationService {
         try {
 //            String redisKey = RedisKeyPrefixEnum.TASK_COOKIE.getRedisKey(param.getTaskId());
 //            RedisUtils.del(redisKey);
-            String url = "https://account.chsi.com.cn/account/getmphonpincode.action?captch={}&mobilePhone={}&optType=REGISTER&ignoremphone=false";
+            String url = "https://account.chsi.com.cn/account/getmphonpincode.action";
+            Map<String, Object> params = new HashMap<>();
+            params.put("captch", param.getPicCode());
+            params.put("mobilePhone", param.getMobile());
+            params.put("optType", "REGISTER");
+            params.put("ignoremphone", "false");
 //            String templateDate = "captch={}&mobilePhone={}&optType=REGISTER&ignoremphone=false";
 //            String date = TemplateUtils.format(templateDate, param.getPicCode(), param.getMobile());
-            response = TaskHttpClient.create(param.getTaskId(), param.getWebsiteName(), RequestType.POST, "chsi_com_cn_04").setFullUrl(url).setRequestCharset(Charset.forName("UTF-8")).invoke();
+            response = TaskHttpClient.create(param.getTaskId(), param.getWebsiteName(), RequestType.POST, "chsi_com_cn_04").setFullUrl(url).setParams(params).setRequestCharset(Charset.forName("UTF-8")).invoke();
             String pageContent = response.getPageContent();
             String str = "学信网已向 " + param.getMobile() + " 发送校验码，请查收";
             if (pageContent.contains(str)) {
