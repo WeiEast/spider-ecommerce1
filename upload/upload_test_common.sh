469338b6ce841ce6541f64b9927e0d7f6b0baea7
zhouxinghai <zhouxinghai@treefinance.com.cn>
2018-01-10 11:37:37 +0800

    增加common打包

---
+++ upload/upload_test_common.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+host="rawdatacentral.saas.test.treefinance.com.cn"
+
+url="http://$host/plugin/uploadPlugin"
+gradle clean install -x test
+
+
+list=`find rawdatacentral-plugin-common/build/libs/rawdatacentral-plugin-common.jar -name '*.jar' | grep -v 'sources'`
+for file in $list
+do
+    curl  -F "file=@$file" $url
+    curl  -F "file=@$file;fileName=rawdatacentral-plugin-common-$HOSTNAME.jar" $url
+done
