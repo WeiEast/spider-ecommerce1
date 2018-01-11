53603ba10561435a3f01e08187cd0249041b2d3d
zhouxinghai <zhouxinghai@treefinance.com.cn>
2018-01-11 14:02:01 +0800

    commom 准生产,预发布上传脚本修改

---
+++ upload/upload_zsc_common.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+host="rawdatacentral.approach.saas.treefinance.com.cn"
+
+
+url="http://$host/plugin/uploadPlugin"
+gradle clean install -x test
+list=`find rawdatacentral-plugin-operator/build/libs/rawdatacentral-plugin-operator.jar -name '*.jar' | grep -v 'sources'`
+for file in $list
+do
+    curl  -F "file=@$file" $url
+    curl  -F "file=@$file;fileName=rawdatacentral-plugin-operator-$HOSTNAME.jar" $url
+done
+
+list=`find rawdatacentral-plugin-operator/src/main/resources -name '*.js'`
+for file in $list
+do
+   parent_name=`dirname $file`
+   file_name=${parent_name##*/}'.'${file##*/}
+   curl  -F "file=@$file;fileName=$file_name" $url
+done
+
