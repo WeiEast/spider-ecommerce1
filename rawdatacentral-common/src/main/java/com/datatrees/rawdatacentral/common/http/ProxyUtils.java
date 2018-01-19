30c64afabb4cad8c92e2505dcf92af6d7882ac2a
zhouxinghai <zhouxinghai@treefinance.com.cn>
2018-01-19 14:11:40 +0800

    归属地修正和归属地支持接口

--- rawdatacentral-common/src/main/java/com/datatrees/rawdatacentral/common/http/ProxyUtils.java
+++ rawdatacentral-common/src/main/java/com/datatrees/rawdatacentral/common/http/ProxyUtils.java
@@ -1,10 +1,15 @@
 package com.datatrees.rawdatacentral.common.http;
 
+import com.alibaba.fastjson.JSON;
 import com.datatrees.rawdatacentral.api.ProxyService;
 import com.datatrees.rawdatacentral.api.RedisService;
 import com.datatrees.rawdatacentral.common.utils.BeanFactoryUtils;
+import com.datatrees.rawdatacentral.common.utils.RedisUtils;
 import com.datatrees.rawdatacentral.domain.enums.RedisKeyPrefixEnum;
+import com.treefinance.proxy.api.ProxyProvider;
+import com.treefinance.proxy.domain.IpLocale;
 import com.treefinance.proxy.domain.Proxy;
+import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -28,9 +33,28 @@ public class ProxyUtils {
     public static boolean getProxyEnable(long taskId) {
         String key = RedisKeyPrefixEnum.TASK_PROXY_ENABLE.getRedisKey(taskId);
         if (redisService.hasKey(key)) {
-           return Boolean.valueOf(redisService.getString(key));
+            return Boolean.valueOf(redisService.getString(key));
         }
         return false;
+    }
+
+    public static IpLocale queryIpLocale(Long taskId, String userIp) {
+        try {
+            if (StringUtils.isBlank(userIp)) {
+                logger.info("query ip locale error,userIp is blank");
+            }
+            ProxyProvider provider = BeanFactoryUtils.getBean(ProxyProvider.class);
+            IpLocale locale = provider.getIpLocale(userIp);
+            if (null != locale) {
+                String k = RedisKeyPrefixEnum.TASK_IP_LOCALE.getRedisKey(taskId);
+                RedisUtils.set(k, JSON.toJSONString(locale), RedisKeyPrefixEnum.TASK_IP_LOCALE.toSeconds());
+            }
+            logger.info("query ip locale,userIp={},locale={}", userIp, JSON.toJSONString(locale));
+            return locale;
+        } catch (Exception e) {
+            logger.info("query ip locale error,userIp={}", userIp, e);
+            return null;
+        }
     }
 
 }
--- rawdatacentral-service/src/main/java/com/datatrees/rawdatacentral/service/impl/CommonPluginApiImpl.java
+++ rawdatacentral-service/src/main/java/com/datatrees/rawdatacentral/service/impl/CommonPluginApiImpl.java
@@ -24,8 +24,7 @@ import com.datatrees.rawdatacentral.domain.result.HttpResult;
 import com.datatrees.rawdatacentral.domain.result.ProcessResult;
 import com.datatrees.rawdatacentral.service.ClassLoaderService;
 import com.datatrees.rawdatacentral.service.WebsiteConfigService;
-import com.treefinance.spider.common.util.http.IpUtils;
-import com.treefinance.spider.common.util.http.domain.IpLocale;
+import com.treefinance.proxy.api.ProxyProvider;
 import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -43,6 +42,8 @@ public class CommonPluginApiImpl implements CommonPluginApi {
     private WebsiteConfigService websiteConfigService;
     @Resource
     private MonitorService       monitorService;
+    @Resource
+    private ProxyProvider        proxyProvider;
 
     @Override
     public HttpResult<Object> init(CommonPluginParam param) {
@@ -86,20 +87,7 @@ public class CommonPluginApiImpl implements CommonPluginApi {
 
                 //设置代理
                 ProxyUtils.setProxyEnable(taskId, param.isProxyEnable());
-                if (StringUtils.isNoneBlank(param.getUserIp())) {
-                    try {
-                        IpLocale locale = IpUtils.queryIpLocale(param.getUserIp());
-                        if (null != locale) {
-                            String k = RedisKeyPrefixEnum.TASK_IP_LOCALE.getRedisKey(param.getTaskId());
-                            RedisUtils.set(k, JSON.toJSONString(locale), RedisKeyPrefixEnum.TASK_IP_LOCALE.toSeconds());
-                        }
-                        logger.info("query user ip locale,taskId={},userIp={},locale={}", param.getTaskId(), param.getUserIp(),
-                                JSON.toJSONString(locale));
-                    } catch (Throwable e) {
-                        logger.error("query ip locale error,taskId={},userIp={}", param.getTaskId(), param.getUserIp(), e);
-                    }
-
-                }
+                ProxyUtils.queryIpLocale(taskId, param.getUserIp());
 
                 //记录登陆开始时间
                 TaskUtils.addTaskShare(taskId, RedisKeyPrefixEnum.START_TIMESTAMP.getRedisKey(param.getFormType()), System.currentTimeMillis() + "");
