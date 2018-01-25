cc4454c8c32e8627208f26a94b38e79b001a75fd
zhouxinghai <zhouxinghai@treefinance.com.cn>
2018-01-25 16:36:46 +0800

    no message

--- rawdatacentral-plugin-common/src/main/java/com/datatrees/rawdatacentral/plugin/util/selenium/SeleniumUtils.java
+++ rawdatacentral-plugin-common/src/main/java/com/datatrees/rawdatacentral/plugin/util/selenium/SeleniumUtils.java
@@ -41,9 +41,9 @@ public class SeleniumUtils {
         if (null != cookies && !cookies.isEmpty()) {
             StringBuilder sb = new StringBuilder();
             for (Cookie cookie : cookies) {
-                sb.append(";").append(cookie.getName()).append("=").append(cookie.getValue());
+                sb.append(" ;").append(cookie.getName()).append("=").append(cookie.getValue());
             }
-            return sb.substring(1);
+            return sb.substring(2);
         }
         return null;
     }
