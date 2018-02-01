4b6279035cc3a2421ad9350e2ccbe5620f79a738
zhangyanjia <zhangyanjia@treefinance.com.cn>
2018-02-01 19:44:35 +0800

    新浪邮箱对外接口增加注解

--- rawdatacentral-api/src/main/java/com/datatrees/rawdatacentral/api/mail/sina/MailServiceApiForSina.java
+++ rawdatacentral-api/src/main/java/com/datatrees/rawdatacentral/api/mail/sina/MailServiceApiForSina.java
@@ -10,9 +10,31 @@ import java.util.Map;
  */
 public interface MailServiceApiForSina {
 
+    /**
+     * 登录初始化接口
+     * 必填参数：taskId
+     * 返回结果
+     * 详见:@see com.datatrees.rawdatacentral.domain.result.HttpResult
+     * @return
+     */
     HttpResult<Object> init(CommonPluginParam param);
 
+    /**
+     * 登录提交接口
+     * 必填参数：taskId，username，password
+     * 选填参数：picCode
+     * 返回结果
+     * 详见:@see com.datatrees.rawdatacentral.domain.result.HttpResult
+     * @return
+     */
     HttpResult<Object> login(CommonPluginParam param);
 
+    /**
+     * 刷新图片接口
+     * 必填参数：taskId
+     * 返回结果
+     * 详见:@see com.datatrees.rawdatacentral.domain.result.HttpResult
+     * @return
+     */
     HttpResult<Object> refeshPicCode(CommonPluginParam param);
 }
