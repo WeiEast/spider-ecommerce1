1b796a0e8fd9b9ae90b3c379a44dd259086afcaf
wangj <wangjiang@treefinance.com.cn>
2018-03-27 18:43:02 +0800

    更新站点状态接口

--- rawdatacentral-web/src/main/java/com/datatrees/rawdatacentral/web/controller/WebsiteGroupController.java
+++ rawdatacentral-web/src/main/java/com/datatrees/rawdatacentral/web/controller/WebsiteGroupController.java
@@ -159,6 +159,7 @@ public class WebsiteGroupController {
     @RequestMapping("/getwebsitenamelist")
     public Object getwebsitenamelist(HttpServletResponse response, String enable, String groupCode, String
             operatorType) {
+        logger.info("getwebsitenamelist() enable={},groupCode={},operatorType={}", enable, groupCode, operatorType);
         HttpResult<Object> result = new HttpResult<>();
         response.setHeader("Access-Control-Allow-Origin", "*");
         return websiteGroupService.getWebsiteNameList(enable, groupCode, operatorType);
