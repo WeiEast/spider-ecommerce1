7a2f88fe88e4613bdd9a9620ecdb8efec3f937db
zhouxinghai <zhouxinghai@treefinance.com.cn>
2018-04-12 15:24:56 +0800

    no message

--- rawdatacentral-plugin-operator/src/main/java/com/datatrees/rawdatacentral/plugin/operator/common/KpiUtils.java
+++ rawdatacentral-plugin-operator/src/main/java/com/datatrees/rawdatacentral/plugin/operator/common/KpiUtils.java
@@ -8,6 +8,7 @@ import com.alibaba.rocketmq.client.producer.DefaultMQProducer;
 import com.alibaba.rocketmq.client.producer.SendResult;
 import com.alibaba.rocketmq.client.producer.SendStatus;
 import com.alibaba.rocketmq.common.message.Message;
+import com.datatrees.rawdatacentral.common.http.TaskUtils;
 import com.datatrees.rawdatacentral.common.utils.BeanFactoryUtils;
 import com.datatrees.rawdatacentral.common.utils.TemplateUtils;
 import com.datatrees.rawdatacentral.domain.constant.AttributeKey;
@@ -23,26 +24,21 @@ public class KpiUtils {
 
     private static final String DEFAULT_CHARSET_NAME = "UTF-8";
 
-    public static void sendKpi(OperatorParam param, String kpiType, String kpiName, String kpiValue, String extra) {
-        Map<String, Object> map = new HashMap<>();
-        map.put(AttributeKey.TASK_ID, param.getTaskId());
-        map.put(AttributeKey.WEBSITE_NAME, param.getWebsiteName());
-        map.put("kpiType", kpiType);
-        map.put("kpiName", kpiName);
-        map.put("kpiValue", kpiValue);
-        map.put("extra", extra);
-        sendMessage(TopicEnum.CRAWLER_MONITOR.getCode(), "kpi", param.getTaskId(), map);
-        logger.info("send kpi data:{}", JSON.toJSONString(map));
+    public static void sendKpi(OperatorParam param, String kpiType, String kpiName, String kpiValue, String kpiDescription) {
+        sendKpi(param.getTaskId(), param.getWebsiteName(), kpiType, kpiName, kpiValue, kpiDescription, null);
     }
 
-    public static void sendKpi(long taskId, String websiteName, String kpiType, String kpiName, String kpiValue, String extra) {
+    public static void sendKpi(long taskId, String websiteName, String kpiType, String kpiName, String kpiValue, String kpiDescription,
+            String extra) {
         Map<String, Object> map = new HashMap<>();
         map.put(AttributeKey.TASK_ID, taskId);
         map.put(AttributeKey.WEBSITE_NAME, websiteName);
+        map.put(AttributeKey.SAAS_ENV, TaskUtils.getSassEnv());
         map.put("kpiType", kpiType);
         map.put("kpiName", kpiName);
         map.put("kpiValue", kpiValue);
         map.put("extra", extra);
+        map.put("kpiDescription", kpiDescription);
         sendMessage(TopicEnum.CRAWLER_MONITOR.getCode(), "kpi", taskId, map);
         logger.info("send kpi data:{}", JSON.toJSONString(map));
     }
