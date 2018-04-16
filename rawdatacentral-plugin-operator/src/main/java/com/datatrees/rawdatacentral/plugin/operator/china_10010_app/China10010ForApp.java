44624bcc94126bad69b5069e6edc955bda78e988
guimeichao <guimeichao@treefinance.com.cn>
2018-04-16 09:55:41 +0800

    联通app修复

--- rawdatacentral-plugin-operator/src/main/java/com/datatrees/rawdatacentral/plugin/operator/china_10010_app/China10010ForApp.java
+++ rawdatacentral-plugin-operator/src/main/java/com/datatrees/rawdatacentral/plugin/operator/china_10010_app/China10010ForApp.java
@@ -138,17 +138,17 @@ public class China10010ForApp implements OperatorPluginService {
                 return result.failure(errorMessage);
             }
 
-            templateUrl = "https://m.client.10010.com/mobileService/operationservice/getUserinfo" +
-                    ".htm?menuId=000200020010&mobile_c_from=query&navUrlCode=2201";
-            data = "timestamp=&desmobile=" + param.getMobile().toString() + "&version=android%405.61";
-            response = TaskHttpClient.create(param, RequestType.POST, "").setFullUrl(templateUrl).setRequestBody(data).invoke();
+            //templateUrl = "https://m.client.10010.com/mobileService/operationservice/getUserinfo" +
+            //        ".htm?menuId=000200020010&mobile_c_from=query&navUrlCode=2201";
+            //data = "timestamp=&desmobile=" + param.getMobile().toString() + "&version=android%405.61";
+            //response = TaskHttpClient.create(param, RequestType.POST, "").setFullUrl(templateUrl).setRequestBody(data).invoke();
+            //
+            //RedisUtils.del(RedisKeyPrefixEnum.TASK_COOKIE.getRedisKey(param.getTaskId()));
 
-            RedisUtils.del(RedisKeyPrefixEnum.TASK_COOKIE.getRedisKey(param.getTaskId()));
-
-            templateUrl = "https://m.client.10010.com/mobileService/logout.htm";
-            data = "version=android%405.61&desmobile=" + param.getMobile().toString();
-            response = TaskHttpClient.create(param, RequestType.POST, "china_10010_app_003").setFullUrl(templateUrl).setRequestBody(data)
-                    .addHeader("User-Agent", "").invoke();
+            //templateUrl = "https://m.client.10010.com/mobileService/logout.htm";
+            //data = "version=android%405.61&desmobile=" + param.getMobile().toString();
+            //response = TaskHttpClient.create(param, RequestType.POST, "china_10010_app_003").setFullUrl(templateUrl).setRequestBody(data)
+            //        .addHeader("User-Agent", "").invoke();
 
             //T = new Random();
             //U = (new StringBuilder()).append(T.nextInt(9)).append("").append(T.nextInt(9)).append("").append(T.nextInt(9)).append("")
@@ -157,33 +157,33 @@ public class China10010ForApp implements OperatorPluginService {
 
             //String passWord = param.getPassword();
             //passWord = (new StringBuilder()).append(passWord).append(U).toString();
-            T = new Random();
-            U = T.nextInt(9) + "" + T.nextInt(9) + "" + T.nextInt(9) + "" + T.nextInt(9) + "" + T.nextInt(9) + "" + T.nextInt(9);
-            templateUrl = "http://m.client.10010.com/mobileService/login.htm";
-            templateData = "deviceOS=android7.0&mobile={}&netWay=WIFI&deviceCode=869782021770312&isRemberPwd=false&version=android%405.61&deviceId" +
-                    "=869782021770312&password={}&keyVersion=&pip=192.168.200.184&provinceChanel=general&appId" +
-                    "=818f10dfa9b3bb4a8e3f4380602d5f47458a6506bbae525a00b4ed19552ac681&deviceModel=Mi+Note+2&deviceBrand=Xiaomi&timestamp={}";
-            data = TemplateUtils.format(templateData, NewEncryptUtilForChina10010App.encode(param.getMobile().toString(), U),
-                    NewEncryptUtilForChina10010App.encode(param.getPassword(), U), new SimpleDateFormat("yyyyMMddHHmmss").format(new Date()));
-            response = TaskHttpClient.create(param, RequestType.POST, "china_10010_app_004").setFullUrl(templateUrl).setRequestBody(data)
-                    .addHeader("User-Agent", "").invoke();
-            json = response.getPageContentForJSON();
-            code = json.getString("code");
-            if (StringUtils.equals(code, "0")) {
+            //T = new Random();
+            //U = T.nextInt(9) + "" + T.nextInt(9) + "" + T.nextInt(9) + "" + T.nextInt(9) + "" + T.nextInt(9) + "" + T.nextInt(9);
+            //templateUrl = "http://m.client.10010.com/mobileService/login.htm";
+            //templateData = "deviceOS=android7.0&mobile={}&netWay=WIFI&deviceCode=869782021770312&isRemberPwd=false&version=android%405.61&deviceId" +
+            //        "=869782021770312&password={}&keyVersion=&pip=192.168.200.184&provinceChanel=general&appId" +
+            //        "=818f10dfa9b3bb4a8e3f4380602d5f47458a6506bbae525a00b4ed19552ac681&deviceModel=Mi+Note+2&deviceBrand=Xiaomi&timestamp={}";
+            //data = TemplateUtils.format(templateData, NewEncryptUtilForChina10010App.encode(param.getMobile().toString(), U),
+            //        NewEncryptUtilForChina10010App.encode(param.getPassword(), U), new SimpleDateFormat("yyyyMMddHHmmss").format(new Date()));
+            //response = TaskHttpClient.create(param, RequestType.POST, "china_10010_app_004").setFullUrl(templateUrl).setRequestBody(data)
+            //        .addHeader("User-Agent", "").invoke();
+            //json = response.getPageContentForJSON();
+            //code = json.getString("code");
+            //if (StringUtils.equals(code, "0")) {
                 String provinceName = PatternUtils.group(response.getPageContent(), "proName\":\"([^\"]+)\"", 1);
                 TaskUtils.addTaskShare(param.getTaskId(), "provinceName", provinceName);
                 logger.info("登陆成功,param={}", param);
                 return result.success();
-            } else if (StringUtils.isNotBlank(code)) {
-                String errorMessage = json.getString("dsc");
-                logger.error("登陆失败,{},param={}", errorMessage, param);
-                return result.failure(errorMessage);
-            } else {
-                logger.error("登陆失败,param={},pageContent={}", param, response.getPageContent());
-                return result.failure(ErrorCode.LOGIN_UNEXPECTED_RESULT);
-            }
+            //} else if (StringUtils.isNotBlank(code)) {
+            //    String errorMessage = json.getString("dsc");
+            //    logger.error("登陆失败,{},param={}", errorMessage, param);
+            //    return result.failure(errorMessage);
+            //} else {
+            //    logger.error("登陆失败,param={},pageContent={}", param, response.getPageContent());
+            //    return result.failure(ErrorCode.LOGIN_UNEXPECTED_RESULT);
+            //}
         } catch (Exception e) {
-            logger.error("基本信息-->校验失败,param={},response={}", param, response, e);
+            logger.error("登录-->校验-->失败,param={},response={}", param, response, e);
             return result.failure(ErrorCode.VALIDATE_UNEXPECTED_RESULT);
         }
     }
