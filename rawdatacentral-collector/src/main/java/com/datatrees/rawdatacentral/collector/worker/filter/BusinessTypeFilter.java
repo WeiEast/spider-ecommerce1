fbaeff5fe9bb8bcda95ac3a3015dc903914f509a
李梁杰 <liliangjie@treefinance.com.cn>
2018-04-28 20:59:30 +0800

    业务标识拦截器新增本地缓存

--- rawdatacentral-collector/src/main/java/com/datatrees/rawdatacentral/collector/worker/filter/BusinessTypeFilter.java
+++ rawdatacentral-collector/src/main/java/com/datatrees/rawdatacentral/collector/worker/filter/BusinessTypeFilter.java
@@ -1,10 +1,15 @@
 package com.datatrees.rawdatacentral.collector.worker.filter;
 
 import javax.annotation.Resource;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.TimeUnit;
 
 import com.datatrees.crawler.core.domain.config.search.SearchTemplateConfig;
 import com.datatrees.crawler.core.processor.page.handler.BusinessTypeFilterHandler;
 import com.datatrees.rawdatacentral.service.AppCrawlerConfigService;
+import com.google.common.cache.Cache;
+import com.google.common.cache.CacheBuilder;
+import com.google.common.util.concurrent.UncheckedExecutionException;
 import com.treefinance.saas.grapserver.facade.model.TaskRO;
 import com.treefinance.saas.grapserver.facade.service.TaskFacade;
 import com.treefinance.saas.knife.result.SaasResult;
@@ -20,7 +25,8 @@ import org.springframework.stereotype.Service;
 @Service("businessTypeFilter")
 public class BusinessTypeFilter implements BusinessTypeFilterHandler {
 
-    private static final Logger logger = LoggerFactory.getLogger(BusinessTypeFilter.class);
+    private static final Logger                 logger     = LoggerFactory.getLogger(BusinessTypeFilter.class);
+    private final        Cache<String, Boolean> localCache = CacheBuilder.newBuilder().expireAfterWrite(10, TimeUnit.MINUTES).expireAfterAccess(1, TimeUnit.MINUTES).softValues().build();
     @Resource
     private TaskFacade              taskFacade;
     @Resource
@@ -31,23 +37,26 @@ public class BusinessTypeFilter implements BusinessTypeFilterHandler {
         if (StringUtils.isBlank(businessType)) {
             return Boolean.FALSE;
         }
-        SaasResult<TaskRO> taskRO = taskFacade.getById(taskId);
-        logger.debug("taskRO is {}", taskRO.getData());
-        if (taskRO.getData() != null) {
-            String appId = taskRO.getData().getAppId();
-            String result = appCrawlerConfigService.getFromRedis(appId, businessType);
-            logger.info("appCrawlerConfig result from redis is {},appId is {},project is {}", result, appId, businessType);
-            //result为null，该业务未配置，默认为抓取
-            if (StringUtils.isBlank(result)) {
-                return Boolean.FALSE;
-            } else if (result.equals("true")) {
-                return Boolean.FALSE;
-            } else if (result.equals("false")) {
-                return Boolean.TRUE;
-            }
-        }
 
-        return false;
+        try {
+            return localCache.get(taskId + "_" + businessType, () -> {
+                SaasResult<TaskRO> taskRO = taskFacade.getById(taskId);
+                logger.debug("taskRO is {}", taskRO.getData());
+
+                if (taskRO.getData() != null) {
+                    String appId = taskRO.getData().getAppId();
+                    String result = appCrawlerConfigService.getFromRedis(appId, businessType);
+                    logger.info("appCrawlerConfig result from redis is {},appId is {},project is {}", result, appId, businessType);
+
+                    //result为null，该业务未配置，默认为抓取
+                    return Boolean.FALSE.toString().equalsIgnoreCase(result);
+                }
+
+                return false;
+            });
+        } catch (ExecutionException e) {
+            throw new UncheckedExecutionException(e);
+        }
     }
 
     public boolean isFilter(SearchTemplateConfig templateConfig, Long taskId) {
