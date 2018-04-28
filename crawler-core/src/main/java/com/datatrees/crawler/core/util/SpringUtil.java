54b61233c0be2a1d18abf51536eb3817eb726027
李梁杰 <liliangjie@treefinance.com.cn>
2018-04-28 18:26:16 +0800

    清理无用日志；清理无用类

--- crawler-core/src/main/java/com/datatrees/crawler/core/util/SpringUtil.java
+++ crawler-core/src/main/java/com/datatrees/crawler/core/util/SpringUtil.java
@@ -1,7 +1,5 @@
 package com.datatrees.crawler.core.util;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.BeansException;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationContextAware;
@@ -10,19 +8,16 @@ import org.springframework.stereotype.Component;
 @Component
 public class SpringUtil implements ApplicationContextAware {
 
-    private static final Logger LOGGER = LoggerFactory.getLogger(SpringUtil.class);
     private static ApplicationContext applicationContext;
 
     @Override
     public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
-        LOGGER.info("applicationContext is {}", applicationContext);
         if (SpringUtil.applicationContext == null) {
             SpringUtil.applicationContext = applicationContext;
         }
     }
 
     public static Object getBeanByBeanName(String beanName) {
-        LOGGER.info("ApplicationContext is {}", applicationContext);
         return applicationContext.getBean(beanName);
     }
 
