dc0558ea9606d30b598980132600f927ff28a7e8
李梁杰 <liliangjie@treefinance.com.cn>
2018-05-07 15:31:20 +0800

    临时调试

--- rawdatacentral-main/src/main/resources/env/daily/logback.groovy
+++ rawdatacentral-main/src/main/resources/env/daily/logback.groovy
@@ -54,3 +54,4 @@ appender("pluginAppender", RollingFileAppender) {
 root(INFO, ["consoleAppender", "sysAppender"])
 logger("plugin_log", INFO, ["pluginAppender"], false)
 logger("com.alibaba.dubbo.monitor.dubbo", OFF)
+logger("com.datatrees.rawdatacentral.service.impl.AppCrawlerConfigServiceImpl", DEBUG)
