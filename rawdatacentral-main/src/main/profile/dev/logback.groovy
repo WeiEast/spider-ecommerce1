bd35ab97ac34b5034a2cef7af6af30a090232b37
李梁杰 <liliangjie@treefinance.com.cn>
2018-06-20 21:24:21 +0800

    调整开发模式下的日志

--- rawdatacentral-main/src/main/profile/dev/logback.groovy
+++ rawdatacentral-main/src/main/profile/dev/logback.groovy
@@ -65,4 +65,6 @@ logger("com.alibaba.dubbo.monitor.dubbo", OFF)
 logger("com.alibaba.dubbo.rpc.protocol.dubbo", OFF)
 logger("com.datatrees.rawdatacentral.submitter.filestore.FileStoreServiceImpl", OFF)
 logger("com.datatrees.rawdatacentral.service.impl.AppCrawlerConfigServiceImpl", DEBUG)
+logger("com.datatrees.crawler.core.processor.operation", DEBUG)
+logger("com.datatrees.crawler.core.processor.extractor", DEBUG)
 
