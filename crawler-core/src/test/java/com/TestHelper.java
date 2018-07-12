8b90f9c50402c76046352d1b49693bc602aa2b92
李梁杰 <liliangjie@treefinance.com.cn>
2018-07-12 12:29:18 +0800

    新增测试用例

---
+++ crawler-core/src/test/java/com/TestHelper.java
@@ -0,0 +1,82 @@
+/*
+ * Copyright © 2015 - 2017 杭州大树网络技术有限公司. All Rights Reserved
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.nio.charset.Charset;
+import java.util.Objects;
+
+import com.datatrees.crawler.core.processor.bean.FileWapper;
+import com.datatrees.crawler.core.util.xml.Impl.XmlConfigParser;
+import com.datatrees.crawler.core.util.xml.exception.ParseException;
+import com.treefinance.crawler.exception.UnexpectedException;
+import org.springframework.util.StreamUtils;
+
+/**
+ * @author Jerry
+ * @since 20:25 06/12/2017
+ */
+public final class TestHelper {
+
+    private TestHelper() {
+    }
+
+    public static <T> T getConfig(String filepath, Class<T> configClass) throws ParseException {
+        String content = getFileContent(filepath);
+
+        return XmlConfigParser.getInstance().parse(content, configClass);
+    }
+
+    public static String getFileContent(String filePath) {
+        try (InputStream inputStream = getInputStream(filePath)) {
+            return StreamUtils.copyToString(inputStream, Charset.defaultCharset());
+        } catch (IOException e) {
+            throw new UnexpectedException("Unexpected exception", e);
+        }
+    }
+
+    public static URL getResource(String filepath) {
+        return TestHelper.class.getClassLoader().getResource(filepath);
+    }
+
+    public static InputStream getInputStream(String filepath) {
+        return TestHelper.class.getClassLoader().getResourceAsStream(filepath);
+    }
+
+    public static File getFile(String path) throws URISyntaxException {
+        URL resource = getResource(path);
+
+        Objects.requireNonNull(resource);
+
+        return new File(resource.toURI());
+    }
+
+    public static FileWapper getPageContent(String path) {
+        return getPageContent(new File(path));
+    }
+
+    public static FileWapper getPageContent(File page) {
+        FileWapper file = new FileWapper();
+        file.setMimeType("text/html");
+        file.setFile(page);
+        return file;
+    }
+}
