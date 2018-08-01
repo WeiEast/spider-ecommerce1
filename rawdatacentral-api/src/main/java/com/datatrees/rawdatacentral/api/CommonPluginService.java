fdbce7ab7fa4c25dd25c58eeaa1bc5a143e679b3
zhouxinghai <1093320666@qq.com>
2018-08-01 11:39:59 +0800

    no message

---
+++ rawdatacentral-api/src/main/java/com/datatrees/rawdatacentral/api/CommonPluginService.java
@@ -0,0 +1,88 @@
+package com.datatrees.rawdatacentral.api;
+
+import java.util.List;
+
+import com.datatrees.spider.share.domain.CommonPluginParam;
+import com.datatrees.spider.share.domain.LoginMessage;
+import com.datatrees.spider.share.domain.ProcessResult;
+import com.datatrees.spider.share.domain.http.Cookie;
+import com.datatrees.spider.share.domain.http.HttpResult;
+
+/**
+ * 通用插件服务
+ */
+public interface CommonPluginApi {
+
+    /**
+     * 初始化
+     * @param param
+     * @return
+     */
+    HttpResult<Object> init(CommonPluginParam param);
+
+    /**
+     * 刷新图片验证码
+     * @param param
+     * @return
+     */
+    HttpResult<Object> refeshPicCode(CommonPluginParam param);
+
+    /**
+     * 刷新短信验证码
+     * @param param
+     * @return
+     */
+    HttpResult<Object> refeshSmsCode(CommonPluginParam param);
+
+    /**
+     * 验证图片验证码
+     * @return
+     */
+    HttpResult<Object> validatePicCode(CommonPluginParam param);
+
+    /**
+     * 登录提交
+     * @return
+     */
+    HttpResult<Object> submit(CommonPluginParam param);
+
+    /**
+     * 自定义方法入口
+     * @return
+     */
+    HttpResult<Object> defineProcess(CommonPluginParam param);
+
+    /**
+     * 查询处理结果
+     * @param processId 处理号
+     * @return
+     */
+    ProcessResult queryProcessResult(long processId);
+
+    /**
+     * 发送登陆成功消息
+     * @param loginMessage
+     */
+    void sendLoginSuccessMsg(LoginMessage loginMessage);
+
+    /**
+     * 发送登陆成功消息
+     * @param loginMessage
+     */
+    void sendLoginSuccessMsg(LoginMessage loginMessage, List<Cookie> cookies);
+
+    /**
+     * 刷新登陆二维码
+     * @param param
+     * @return
+     */
+    HttpResult<Object> refeshQRCode(CommonPluginParam param);
+
+    /**
+     * 查询二维码状态
+     * @param param
+     * @return
+     */
+    HttpResult<Object> queryQRStatus(CommonPluginParam param);
+
+}
