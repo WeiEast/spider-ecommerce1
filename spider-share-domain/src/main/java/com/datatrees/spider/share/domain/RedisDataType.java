8f6f585c61d54f8806e5dd0dc16c798f55557f52
zhouxinghai <1093320666@qq.com>
2018-08-06 17:02:03 +0800

    no message

---
+++ spider-share-domain/src/main/java/com/datatrees/spider/share/domain/RedisDataType.java
@@ -0,0 +1,14 @@
+package com.datatrees.spider.share.domain;
+
+/**
+ * redis key type
+ */
+public class RedisDataType {
+
+    public static final String NONE   = "none";//key不存在
+    public static final String STRING = "string";//string类型
+    public static final String HASH   = "hash";//map类型
+    public static final String LIST   = "list";//list类型
+    public static final String SET    = "set";//set类型
+    public static final String ZSET   = "zset";//有序set类型
+}
