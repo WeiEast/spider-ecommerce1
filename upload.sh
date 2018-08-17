f6243593cf61d6f1e28ce4fffcf8d25cf3345b27
zhouxinghai <1093320666@qq.com>
2018-08-17 10:55:27 +0800

    no message

--- upload.sh
+++ upload.sh
@@ -3,5 +3,6 @@ gradle clean spider-share-domain:upload  spider-share-api:upload spider-share-co
 gradle spider-operator-domain:upload  spider-operator-api:upload -x test
 gradle spider-bank-domain:upload  spider-bank-api:upload -x test
 gradle spider-ecommerce-domain:upload  spider-ecommerce-api:upload -x test
+gradle spider-extra-domain:upload  spider-extra-api:upload -x test
 
 
